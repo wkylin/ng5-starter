eb92291f5252f8094fe0ff5e32877a1ea255167b
wkylin.w <wkylin.w@gmail.com>
2018-01-11 22:07:11 +0800

    Customize the Initial Loading Screen in Angular

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -15,9 +15,6 @@ export class HomeComponent implements OnInit {
   state = 'small';
 
   values = '';
-
-  number = '';
-
   constructor(private http: HttpClient, private postsService: PostsService) {
   }
 
@@ -44,46 +41,5 @@ export class HomeComponent implements OnInit {
 
   removeItem() {
     this.posts.pop();
-  }
-
-  onKey(event: KeyboardEvent) { // with type info
-    // this.values += (<HTMLInputElement>event.target).value + ' | ';
-    if (/^\d+\.?\d{0,2}$/.test((<HTMLInputElement>event.target).value)) {
-      this.values = (<HTMLInputElement>event.target).value;
-    } else {
-      this.values = this.values.substring(0, (<HTMLInputElement>event.target).value.length - 1);
-    }
-  }
-
-  // formatFixed(number) {
-  formatFixed(event) {
-    let numF = event.value.toString();
-    if (numF.indexOf('.') !== -1) {
-      numF = numF.substring(0, numF.indexOf('.') + 3);
-      const temArray = numF.split('.');
-      const temLast = temArray[1];
-      let tem = '';
-      if (temLast.indexOf('.') !== -1) {
-        tem = temLast.substring(0, temLast.indexOf('.'));
-      } else {
-        tem = temLast;
-      }
-      numF = temArray[0] + '.' + tem;
-    }
-    numF = parseFloat(numF).toFixed(3);
-    if (numF === 'NaN') {
-      return '';
-    }
-    if (numF.indexOf('.') === -1) {
-      numF = numF + '.00';
-    } else if (numF.substr(-2, 1) === '.') {
-      numF = numF + '0';
-    } else if (numF.substr(-3, 1) === '.') {
-      numF = numF;
-    } else {
-      numF = numF.substring(0, numF.length - 1);
-    }
-    // return numF;
-    this.values = numF;
   }
 }
