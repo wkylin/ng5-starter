33c79773ab11fc4c1fa6f8ad4629bed6790ba76f
wkylin.w <wkylin.w@gmail.com>
2018-01-20 00:21:32 +0800

    route animation...

--- src/app/modules/my/index/index.component.ts
+++ src/app/modules/my/index/index.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
-import {SlideInOutAnimation} from '../../../router-animations';
+import { SlideInOutAnimation } from '../../../router-animations';
 
 @Component({
-  selector: 'app-index',
-  templateUrl: './index.component.html',
-  styleUrls: ['./index.component.scss'],
-  animations: [SlideInOutAnimation]
+    selector: 'app-index',
+    templateUrl: './index.component.html',
+    styleUrls: ['./index.component.scss'],
+    animations: [SlideInOutAnimation]
 })
 export class IndexComponent implements OnInit {
-  @HostBinding('@slideInOutAnimation') routeAnimation = true;
-  @HostBinding('style.display') display = 'block';
-  constructor() { }
+    @HostBinding('@slideInOutAnimation') routeAnimation = true;
+    @HostBinding('style.display') display = 'block';
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
 
 }
