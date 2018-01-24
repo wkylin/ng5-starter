fdb796d50f9e57392cd583eb5665c42e67e24b38
wkylin.w <wkylin.w@gmail.com>
2018-01-24 23:30:43 +0800

    ngx-loading...

--- src/app/common/my-preloading-strategy.ts
+++ src/app/common/my-preloading-strategy.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
 export class AppCustomPreloading implements PreloadingStrategy {
-    preload(route: Route, load: Function): Observable<any> {
-        return route.data && route.data.preload ? load() : of(null);
-    }
+  preload(route: Route, load: Function): Observable<any> {
+    return route.data && route.data.preload ? load() : of(null);
+  }
 }
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -5,36 +5,42 @@ import { SettingComponent } from '../modules/my/setting/setting.component';
 
 @Injectable()
 export class AuthGuard implements CanLoad, CanActivate, CanActivateChild, CanDeactivate<SettingComponent> {
-
-    constructor(private authService: AuthService) {
+  
+  constructor(private authService: AuthService) {
+  }
+  
+  /**
+   * 验证是否有权限加载一个异步模块
+   */
+  canLoad() {
+    // 在真实的应用里面需要写一个Service到后端去验证权限
+    return this.authService.canLoad();
+  }
+  
+  /**
+   * 验证路由是否可以激活
+   */
+  canActivate() {
+    return this.authService.canActivate();
+  }
+  
+  
+  /**
+   * 验证子路由是否可以激活
+   */
+  canActivateChild() {
+    return true;
+  }
+  
+  /**
+   * 验证路由是否可以离开
+   * @param {SettingComponent} target
+   * @returns {boolean}
+   */
+  canDeactivate(target: SettingComponent) {
+    if (target.hasChanges()) {
+      return window.confirm('Do you really want to cancel?');
     }
-
-    /**
-     * 验证是否有权限加载一个异步模块
-     */
-    canLoad() {
-        // 在真实的应用里面需要写一个Service到后端去验证权限
-        return this.authService.canLoad();
-    }
-
-    /**
-     * 验证路由是否可以激活
-     */
-    canActivate() {
-        return this.authService.canActivate();
-    }
-
-    /**
-     * 验证子路由是否可以激活
-     */
-    canActivateChild() {
-        return true;
-    }
-
-    canDeactivate(target: SettingComponent) {
-        if (target.hasChanges()) {
-            return window.confirm('Do you really want to cancel?');
-        }
-        return true;
-    }
+    return true;
+  }
 }
