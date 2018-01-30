2c3eca4993fef602dd6b510efdd37670dbf27e57
wkylin <wkylin@qq.com>
2018-01-30 13:50:46 +0800

    http-client-and-http-interceptors...

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,10 +3,10 @@ import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthService {
-  
+
   constructor(public router: Router) {
   }
-  
+
   public login() {
     const authResult = {
       expiresIn: 60,
@@ -15,46 +15,46 @@ export class AuthService {
     this.setSession(authResult);
     this.router.navigate(['/my/index']);
   }
-  
+
   private setSession(authResult): void {
     // const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
     localStorage.setItem('access_token', authResult.accessToken);
     // localStorage.setItem('expires_at', expiresAt);
   }
-  
+
   public logout(): void {
     localStorage.removeItem('access_token');
     // localStorage.removeItem('expires_at');
     this.router.navigate(['/sign/login']);
   }
-  
+
   public isAuthenticated(): boolean {
     // const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
     // return new Date().getTime() < expiresAt;
     const accessToken = localStorage.getItem('access_token');
     return accessToken ? true : false;
   }
-  
+
   canLoad(): boolean {
     // Http调用后端的服务检查授权
     return true;
   }
-  
+
   canActivate(): boolean {
     // Http调用后端的服务检查授权
-  
-    let isLogin = localStorage.getItem('access_token') ? true : false;
-  
+
+    const isLogin = localStorage.getItem('access_token') ? true : false;
+
     if (!isLogin) {
       this.router.navigate(['/sign/login']);
     }
-  
+
     return isLogin;
     // return true;
   }
-  
+
   canDeactivate(): boolean {
-    let isSave = window.confirm('Do you really want to cancel?');
+    const isSave = window.confirm('Do you really want to cancel?');
     return isSave;
   }
 }
